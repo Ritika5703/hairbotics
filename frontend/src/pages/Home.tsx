91603ed076aa1a56c12f2caec943fe9b3ddeccd2
Ritika Mishra <mishraritika579@gmail.com>
2025-04-05 23:57:26 +0530

    Navbar

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
